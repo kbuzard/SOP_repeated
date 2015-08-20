ae51fab95c732c355676f04b3a040ce865d2b42e
kbuzard <kbuzard@gmail.com>
2015-08-19 23:23:30 -0400

    modify R program to parameterize gamma exponent, add ea

--- solve_leg_constraint.R
+++ solve_leg_constraint.R
@@ -1,8 +1,9 @@
 #given gamma above, solve for trade war effort level, etw$root
-etw <- uniroot(function(cn) (8/49*(1+(8*(1 + cn^.3)-5)/(68-8*(1 + cn^.3)))*(8*63*.2*(cn^(-.8)))/((68-8*(1 + cn^.3))^2))-1, lower=0, upper = 1, tol = 0.00001, maxiter = 1000)
+E=.2
+etw <- uniroot(function(cn) (8/49*(1+(8*(1 + cn^E)-5)/(68-8*(1 + cn^E)))*(8*63*.2*(cn^(-.8)))/((68-8*(1 + cn^E))^2))-1, lower=0, upper = 1, tol = 0.00001, maxiter = 1000)
 
 # given trade war effort level, compute trade war tariff, 'ttw'
-ttw = (8*(1 + etw$root^.3)-5)/(68-8*(1 + etw$root^.3))
+ttw = (8*(1 + etw$root^E)-5)/(68-8*(1 + etw$root^E))
 
 #producer and consumer surpluses (in X and Y industries) and tariff revenue at trade war tariff
 PSxtw = ((2 +2*ttw)^2)/49
@@ -11,13 +12,9 @@ TRtw = (ttw - 6*ttw^2)/7
 CSytw = ((3 +3*ttw)^2)/98
 PSytw = ((4 -3*ttw)^2)/98
 
-# total discounting for legislature
-d = .99
-T=100
-fd = (d -d^(T+1))/(1-d)
-
 # trade agreement tariff
-ta = .052
+ta = .08
+ea = ((68*ta + 5)/(8+8*ta)-1)^(1/E)
 
 #producer and consumer surpluses (in X and Y industries) and tariff revenue at trade agreement tariff
 PSxta = ((2 +2*ta)^2)/49
@@ -26,18 +23,23 @@ TRta = (ta - 6*ta^2)/7
 CSyta = ((3 +3*ta)^2)/98
 PSyta = ((4 -3*ta)^2)/98
 
+# total discounting for legislature
+d = .99
+T=30
+fd = (d -d^(T+1))/(1-d)
+
 #legislature's incentive constraint, with all e's set to the break e (instead of e_a and e_tw)
-f <- function (e) fd*(CSxta + (1 + e^.3)*PSxta + CSyta + PSyta + TRta) + (CSxta + (1 + e^.3)*PSxta + CSyta + PSyta + TRta) - fd*(CSxtw + (1 + e^.3)*PSxtw + CSytw + PSytw + TRtw) - ((1 + e^.3)*((2 +2*(8*(1 + e^.3)-5)/(68-8*(1 + e^.3)))^2)/49 + .5*((3 -4*(8*(1 + e^.3)-5)/(68-8*(1 + e^.3)))^2)/49 + ((8*(1 + e^.3)-5)/(68-8*(1 + e^.3)) - 6*(8*(1 + e^.3)-5)/(68-8*(1 + e^.3))^2)/7 + CSyta + PSyta)
+f <- function (e) fd*(CSxta + (1 + e^E)*PSxta + CSyta + PSyta + TRta) + (CSxta + (1 + e^E)*PSxta + CSyta + PSyta + TRta) - fd*(CSxtw + (1 + e^E)*PSxtw + CSytw + PSytw + TRtw) - ((1 + e^E)*((2 +2*(8*(1 + e^E)-5)/(68-8*(1 + e^E)))^2)/49 + .5*((3 -4*(8*(1 + e^E)-5)/(68-8*(1 + e^E)))^2)/49 + ((8*(1 + e^E)-5)/(68-8*(1 + e^E)) - 6*(8*(1 + e^E)-5)/(68-8*(1 + e^E))^2)/7 + CSyta + PSyta)
 
 #solve for effort level that makes legislature indifferent (\ov{e} in text)
 ove = uniroot(f, lower = 0, upper = .1, tol = 0.00001, maxiter = 1000)
 
 #discount term for lobby. length of punishment is same as for leg, but delta can be different
-dl = .80
+dl = .99
 fdl = (dl -dl^(T+1))/(1-dl)
 
 #This is benefit to lobby
-bl = ((2 +2*(8*(1 + ove$root^.3)-5)/(68-8*(1 + ove$root^.3)))^2)/49 - PSxta + fdl*(PSxtw - etw$root - PSxta)
+bl = ((2 +2*(8*(1 + ove$root^E)-5)/(68-8*(1 + ove$root^E)))^2)/49 - PSxta +ea + fdl*(PSxtw - etw$root - PSxta +ea)
 
 #lobby's constraint (need this to be negative, as close to zero as possible)'
 ove$root - bl
