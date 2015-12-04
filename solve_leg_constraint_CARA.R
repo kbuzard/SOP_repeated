420a22466b922465f939261d1f448a23257b4424
Kristy Buzard <kbuzard@gmail.com>
2015-12-04 12:33:34 -0500

    fix intercept on gamma from 2.25 to 2

--- solve_leg_constraint_CARA.R
+++ solve_leg_constraint_CARA.R
@@ -1,9 +1,9 @@
 #given gamma above, solve for trade war effort level, etw$root
-a = 38
-etw <- uniroot(function(cn) 8/49*(1+(8*(2.25-exp(-a*cn))-5)/(68-8*(2.25-exp(-a*cn))))*(8*63)/((68-8*(2.25-exp(-a*cn)))^2)*a*exp(-a*cn)-1, lower=0, upper = .3, tol = 0.00001, maxiter = 1000)
+a = 42
+etw <- uniroot(function(cn) 8/49*(1+(8*(2-exp(-a*cn))-5)/(68-8*(2-exp(-a*cn))))*(8*63)/((68-8*(2-exp(-a*cn)))^2)*a*exp(-a*cn)-1, lower=0, upper = .3, tol = 0.00001, maxiter = 1000)
 
 # given trade war effort level, compute trade war tariff, 'ttw'
-ttw = (8*(2.25 - exp(-a*etw$root))-5)/(68-8*(2.25 - exp(-a*etw$root)))
+ttw = (8*(2 - exp(-a*etw$root))-5)/(68-8*(2 - exp(-a*etw$root)))
 
 #producer and consumer surpluses (in X and Y industries) and tariff revenue at trade war tariff
 PSxtw = ((2 +2*ttw)^2)/49
@@ -36,11 +36,11 @@ for (j in 1:length(ta)) {
   t = ta[j]
   
 #legislature's incentive constraint, with all e's set to the break e (instead of e_a and e_tw)
-f <- function (e) fd*(CSxta[j] + (2.25 - exp(-a*e))*PSxta[j] + CSyta[j] + PSyta[j] + TRta[j]) + (CSxta[j] + (2.25 - exp(-a*e))*PSxta[j] + CSyta[j] + PSyta[j] + TRta[j]) - fd*(CSxtw + (2.25 - exp(-a*e))*PSxtw + CSytw + PSytw + TRtw) - ((2.25 - exp(-a*e))*((2 +2*(8*(2.25 - exp(-a*e))-5)/(68-8*(2.25 - exp(-a*e))))^2)/49 + .5*((3 -4*(8*(2.25 - exp(-a*e))-5)/(68-8*(2.25 - exp(-a*e))))^2)/49 + (8*(2.25 - exp(-a*e))-5)/(68-8*(2.25 - exp(-a*e)))/7 - 6/7*((8*(2.25 - exp(-a*e))-5)/(68-8*(2.25 - exp(-a*e))))^2 + CSyta[j] + PSyta[j])
+f <- function (e) fd*(CSxta[j] + (2 - exp(-a*e))*PSxta[j] + CSyta[j] + PSyta[j] + TRta[j]) + (CSxta[j] + (2 - exp(-a*e))*PSxta[j] + CSyta[j] + PSyta[j] + TRta[j]) - fd*(CSxtw + (2 - exp(-a*e))*PSxtw + CSytw + PSytw + TRtw) - ((2 - exp(-a*e))*((2 +2*(8*(2 - exp(-a*e))-5)/(68-8*(2 - exp(-a*e))))^2)/49 + .5*((3 -4*(8*(2 - exp(-a*e))-5)/(68-8*(2 - exp(-a*e))))^2)/49 + (8*(2 - exp(-a*e))-5)/(68-8*(2 - exp(-a*e)))/7 - 6/7*((8*(2 - exp(-a*e))-5)/(68-8*(2 - exp(-a*e))))^2 + CSyta[j] + PSyta[j])
 ove = uniroot(f, lower = ea[j], upper = .10, tol = 0.000001, maxiter = 1000) #solve for effort level that makes legislature indifferent (\ov{e} in text)
 
 #This is benefit to lobby
-b1 = ((2 +2*(8*(2.25 - exp(-a*ove$root))-5)/(68-8*(2.25 - exp(-a*ove$root))))^2)/49
+b1 = ((2 +2*(8*(2 - exp(-a*ove$root))-5)/(68-8*(2 - exp(-a*ove$root))))^2)/49
 b2 = - PSxta[j] +ea[j] + fdl*(PSxtw - etw$root - PSxta[j] +ea[j])
 
 out[j,1] = ta[j] #trade agreement tariff
